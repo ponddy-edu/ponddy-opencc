bd31e5c8b4789670964f38ca9e0df5d27fe5ae8c
SunCooper <coopersun@ponddy-edu.com>
2019-07-10 17:22:53 +0800

    Add 黎明

--- example.py
+++ example.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
         print('Require Python3 to run')
         sys.exit(0)
 
-    words_t = '校長發動罷工'
-    words_s = '校长发动罢工'
+    words_t = '天已黎明了'
+    words_s = '天已黎明了'
     openCC_ponddy_t2s = OpenCC('ponddy_t2s')
     tword_ponddy_t2s_result = openCC_ponddy_t2s.convert(words_t)
     sword_ponddy_t2s_result = openCC_ponddy_t2s.convert(words_s)
