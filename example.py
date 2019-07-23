cd1d55fa5b21b620084cd68116516bf1b75a08ca
SunCooper <coopersun@ponddy-edu.com>
2019-07-23 16:33:20 +0800

    Remove 著 in simp

--- example.py
+++ example.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
         print('Require Python3 to run')
         sys.exit(0)
 
-    words_t = '天已黎明了'
-    words_s = '天已黎明了'
+    words_t = '看著什麼'
+    words_s = '看着什麼'
     openCC_ponddy_t2s = OpenCC('ponddy_t2s')
     tword_ponddy_t2s_result = openCC_ponddy_t2s.convert(words_t)
     sword_ponddy_t2s_result = openCC_ponddy_t2s.convert(words_s)
