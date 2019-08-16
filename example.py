2684f5424c883f75d87373712cc33a66018b10e4
SunCooper <coopersun@ponddy-edu.com>
2019-08-16 11:29:19 +0800

    Add vocabulary 是只有 to s2t

--- example.py
+++ example.py
@@ -9,8 +9,8 @@ if __name__ == '__main__':
         print('Require Python3 to run')
         sys.exit(0)
 
-    words_t = '梁偉'
-    words_s = '梁偉'
+    words_t = '家庭 规模 缩小 是 其中 最 大 的 改变 。 现在 一 个 中国 家庭 平均 人数 不到 四 人 ， 过去 三代同堂 的 家庭 已经 很 少见 ， 取代 传统 家庭 的 是 只有 父母 跟 孩子 住 在 一起 的 核心 家庭 。 这 可能 是 因为 政府 的 鼓励 政策 ； 可能 是 因为 城市 工作 机会 多 ， 很 少 人 愿意 回 乡下 住 。'
+    words_s = '家庭 规模 缩小 是 其中 最 大 的 改变 。 现在 一 个 中国 家庭 平均 人数 不到 四 人 ， 过去 三代同堂 的 家庭 已经 很 少见 ， 取代 传统 家庭 的 是 只有 父母 跟 孩子 住 在 一起 的 核心 家庭 。 这 可能 是 因为 政府 的 鼓励 政策 ； 可能 是 因为 城市 工作 机会 多 ， 很 少 人 愿意 回 乡下 住 。'
     openCC_ponddy_t2s = OpenCC('ponddy_t2s')
     tword_ponddy_t2s_result = openCC_ponddy_t2s.convert(words_t)
     sword_ponddy_t2s_result = openCC_ponddy_t2s.convert(words_s)
